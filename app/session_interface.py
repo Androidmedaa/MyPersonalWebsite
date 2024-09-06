f6101fad2a1d306056cb2c1e55d3370ce3a4e0a4
Androidmedaa <dilberozer2096@gmail.com>
2024-09-06 21:41:20 +0300

    Update session_interface.py

--- app/session_interface.py
+++ app/session_interface.py
@@ -1,12 +1,9 @@
-# kendi interfacemizi olusturmadan once kendi nesnemizi olsuturmamız gerekiyor
-import uuid #eşsiz bir id vermek için
+# Before creating our own interface, we need to create our own object.
+import uuid #to give a unique id
 import json
 from itsdangerous import signer, BadSignature, want_bytes
 from flask.sessions import SessionMixin, SessionInterface
-
 from itsdangerous import Signer, BadSignature, want_bytes
-
-#SessionMixin mapping ten olusturulmus bu yuzden aynı desene sahip
 
 class MySession(dict,SessionMixin): #MySession sınıfı
     def __init__(self, initial=None,sessionId=None):
@@ -24,8 +21,6 @@ class MySession(dict,SessionMixin): #MySession sınıfı
     def __delitem__(self,key):
         super(MySession,self).__delitem__(key)
 
-
-
 class MySessionInterface(SessionInterface):
     session_class=MySession
     salt='my-session'
@@ -35,16 +30,12 @@ class MySessionInterface(SessionInterface):
 
     #ben bu degerleri bir sozlukte kaydedicem
     container=dict()
-
     def __init__(self):
         pass
-
     def open_session(self,app,request):
         #bize requesti verdiği için cookie den id yi alalım
         signer=Signer(app.secret_key, salt=self.salt,key_derivation='hmac' )
-
         signedSessionId = request.cookies.get(app.config['SESSION_COOKIE_NAME'])
-
         if not signedSessionId: #eger yoksa
             sessionId=str(uuid.uuid4())
             return self.session_class(sessionId=sessionId)
@@ -54,31 +45,21 @@ class MySessionInterface(SessionInterface):
         except BadSignature:
             sessionId=str(uuid.uuid4())
             return self.session_class(sessionId=sessionId)
-
         initialSessionValueAsJson= self.container.get(sessionId)
         
         try:
             initialSessionValue=json.loads(initialSessionValueAsJson)
-        
         except:
             sessionId=str(uuid.uuid4())
             return self.session_class(sessionId=sessionId)
-
-
         return self.session_class(initialSessionValue,sessionId=sessionId)
 
     def save_session(self,app,session,response):
         if session is not None and hasattr(session, 'sessionId'):
-            sessionAsJson= json.dumps(dict(session)) #json modunda kaydedicez
-            # şimdi imzalama yonteminin aynısın burda da kullanıcaz
-
+            sessionAsJson= json.dumps(dict(session)) #save in json mode
             self.container[session.sessionId]=sessionAsJson
-
             signer=Signer(app.secret_key,salt=self.salt, key_derivation='hmac')
             signedSessionId=signer.sign(session.sessionId).decode()
-
             response.set_cookie(app.config['SESSION_COOKIE_NAME'],signedSessionId)#key,value
-
         else:
             pass
-
